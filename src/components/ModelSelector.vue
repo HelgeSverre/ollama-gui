35ebd4e0cedb75cc21a174818cde9c1072346a98
Helge Sverre <helge.sverre@gmail.com>
2023-10-14 23:14:56 +0200

    wip

--- src/components/ModelSelector.vue
+++ src/components/ModelSelector.vue
@@ -3,13 +3,13 @@ import { IconRefresh } from '@tabler/icons-vue'
 import { useChats } from '../services/chat.ts'
 import { useAI } from '../services/useAI.ts'
 
-const { hasMessages, activeChat, switchModel, isCurrentChatPopulated } = useChats()
+const { activeChat, switchModel, hasMessages } = useChats()
 const { refreshModels, availableModels } = useAI()
 
 const handleModelChange = (event: Event) => {
   const wip = event.target as HTMLSelectElement
-  console.log('swiatch', wip.value)
-  switchModel(wip.value)
+  console.log('switch', wip.value)
+  // switchModel(wip.value)
 }
 </script>
 
@@ -18,7 +18,7 @@ const handleModelChange = (event: Event) => {
     <div class="px-2 py-4 text-zinc-800 dark:text-zinc-200">
       <div class="h-10 inline-flex gap-2 items-center">
         <select
-          :disabled="isCurrentChatPopulated"
+          :disabled="hasMessages"
           :value="activeChat?.model"
           @change="handleModelChange"
           class="w-full min-w-full cursor-pointer rounded-lg border-r-8 border-transparent bg-zinc-200 py-2 h-full pl-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-600 dark:bg-zinc-700 disabled:opacity-50"
@@ -30,7 +30,7 @@ const handleModelChange = (event: Event) => {
         </select>
 
         <button
-          :disabled="isCurrentChatPopulated"
+          :disabled="hasMessages"
           title="Refresh available models"
           @click="refreshModels"
           class="p-3 inline-flex items-center justify-center h-full rounded-lg bg-zinc-200 text-sm focus:outline-none border-none focus:ring-2 focus:ring-blue-600 dark:bg-zinc-700 disabled:opacity-50"
