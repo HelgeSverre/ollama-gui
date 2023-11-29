7b1c6012628b7dfa35eac42296853be2ccde0172
Helge Sverre <helge.sverre@gmail.com>
2023-11-29 06:48:33 +0100

    forward some props to the textinput (autofocus, blur event)

--- src/components/Inputs/TextInput.vue
+++ src/components/Inputs/TextInput.vue
@@ -2,16 +2,19 @@
 type Props = {
   label?: string
   type?: string
+  autofocus?: boolean
   placeholder?: string
   modelValue: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: 'text',
+  autofocus: false,
 })
 
 const emit = defineEmits<{
   (event: 'update:modelValue', value: string): void
+  (event: 'blur'): void
 }>()
 </script>
 
@@ -23,8 +26,10 @@ const emit = defineEmits<{
     <input
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
+      @blur="$emit('blur')"
       :type="type"
-      class="block w-full rounded-lg bg-zinc-200 p-2.5 text-xs focus:outline-none focus:ring-2 focus:ring-blue-600 dark:bg-zinc-800 dark:placeholder-zinc-400 dark:focus:ring-blue-600"
+      :autofocus="autofocus"
+      class="block w-full rounded-lg bg-zinc-200 p-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-600 dark:bg-zinc-800 dark:placeholder-zinc-400 dark:focus:ring-blue-600"
       :placeholder="placeholder"
     />
   </div>
