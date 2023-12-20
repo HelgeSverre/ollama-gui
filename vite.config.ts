a5f667e22ea5fa0ea65db277f85ba00ed10b3c2c
Helge Sverre <helge.sverre@gmail.com>
2023-12-20 19:48:28 +0100

    aider: Set chunk size warning limit to 500 kBs and split vendor libraries into separate chunks in `vite.config.ts`.
    # Aider chat conversation:
    
    USER: getting this when running yarn build `package.json` `src/main.ts` `vite.config.ts` !) Some chunks are larger than 500 kBs after minification. Consider:
    - Using dynamic import() to code-split the application
    - Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/configuration-options/#output-manualchunks
    - Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.
    ✓ built in 8.41s
    ✨  Done in 12.30s.
    
    ASSISTANT: To address the warning about chunk sizes, we can modify the `vite.config.ts` file to include `build.rollupOptions.output.manualChunks` to split vendor libraries into separate chunks. This can help reduce the size of the main chunk and improve loading times.
    
    Here's the diff for the `vite.config.ts` file:
    
    ```diff
    --- vite.config.ts
    +++ vite.config.ts
    @@ ... @@
     export default defineConfig({
       plugins: [vue()],
    +  build: {
    +    rollupOptions: {
    +      output: {
    +        manualChunks(id) {
    +          if (id.includes('node_modules')) {
    +            return 'vendor';
    +          }
    +        }
    +      }
    +    },
    +    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit to 500 kBs
    +  },
     })
    ```
    
    This change will instruct Vite to separate the vendor libraries into a `vendor` chunk and set the chunk size warning limit to 500 kBs. If you have specific libraries that you want to split into their own chunks, you can modify the `manualChunks` function accordingly.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit to 500 kBs
+  },
 })
