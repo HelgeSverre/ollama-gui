2be40755d9bea06e948bd8c299fdc255a96c2968
Helge Sverre <helge.sverre@gmail.com>
2023-12-20 20:53:46 +0100

    follow scroll until user interupts, then dont follow scroll until user scrolls to bottom again.

--- src/components/ChatMessages.vue
+++ src/components/ChatMessages.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, onUpdated, ref } from 'vue'
+import { computed, nextTick, onMounted, onUnmounted, onUpdated, ref, watch } from 'vue'
 import ChatMessage from './ChatMessage.vue'
 import { useChats } from '../services/chat.ts'
 import { debugMode } from '../services/appConfig.ts'
 
 const { messages } = useChats()
 const chatElement = ref<HTMLElement>()
+const userInterferedWithScroll = ref(false)
+
+const isAtBottom = () => {
+  if (!chatElement.value) return false
+
+  const { scrollTop, scrollHeight, clientHeight } = chatElement.value
+  return scrollHeight - scrollTop <= clientHeight + 10 // 10 is a small threshold
+}
+
+const handleUserScroll = () => {
+  userInterferedWithScroll.value = !isAtBottom()
+}
 
 const scrollToBottom = () => {
+  if (userInterferedWithScroll.value) return
+
   nextTick(() => {
     if (chatElement.value) {
       chatElement.value.scrollTop = chatElement.value.scrollHeight
@@ -15,8 +29,20 @@ const scrollToBottom = () => {
   })
 }
 
-onMounted(() => scrollToBottom())
+onMounted(() => {
+  scrollToBottom()
+  chatElement.value?.addEventListener('scroll', handleUserScroll)
+})
+
 onUpdated(() => scrollToBottom())
+
+watch(messages, () => {
+  if (isAtBottom()) {
+    userInterferedWithScroll.value = false
+  }
+})
+
+onUnmounted(() => chatElement.value?.removeEventListener('scroll', handleUserScroll))
 
 const visibleMessages = computed(() =>
   debugMode.value ? messages?.value : messages?.value.filter((m) => m.role != 'system'),
