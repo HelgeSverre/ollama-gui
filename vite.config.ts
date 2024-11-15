2960a823d5816b286da98bb3cf05f0e36c898c7e
Helge Sverre <helge.sverre@gmail.com>
2024-11-15 10:03:20 +0100

    dont be strict about port.

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   build: { chunkSizeWarningLimit: 1500, },
   preview: {
     port: 8080,
-    strictPort: true,
+    strictPort: false,
   },
   server: {
     port: 8080,
-    strictPort: true,
+    strictPort: false,
     host: "0.0.0.0",
     origin: "http://0.0.0.0:8080",
   },
