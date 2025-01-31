cc478edc3f91bfcf567b4d1dec4c009372b5a432
talperets <tal.perets3@gmail.com>
2025-01-31 11:42:07 +0100

    Adding Connection error message on init

--- src/services/testConnection.ts
+++ src/services/testConnection.ts
@@ -1,7 +1,11 @@
-// Test Connection
-const testConnection = async (): Promise<Boolean>=> {
-    const response = await fetch("http://localhost:11434/")
-    return response.ok
+const testConnection = async (): Promise<boolean> => {
+  try {
+    const response = await fetch("http://localhost:11434/");
+    return response.ok;
+  } catch (error) {
+    console.error("Connection failed:", error);
+    return false;
   }
+};
 
-export default testConnection
+export default testConnection;
